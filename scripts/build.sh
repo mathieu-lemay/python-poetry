07f62fc891a9b55bd34db9444235d8f126096977
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-10-06 09:16:21 -0400

    Add images for python 3.10

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-VERSIONS=("2.7" "3.5" "3.6" "3.7" "3.8" "3.9")
-LATEST="3.9"
+VERSIONS=("2.7" "3.5" "3.6" "3.7" "3.8" "3.9" "3.10")
+LATEST="3.10"
 POETRY_VERSION="1.1.11"
 
 error() { printf "\\e[35m[ERROR]\\e[0m %s\\n" "$*" >&2 ; exit 1 ; }
