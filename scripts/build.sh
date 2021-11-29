547d5ef25b1e4b0061e975a70f28a0c54dfec642
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-11-29 11:28:57 -0500

    Update poetry to 1.1.12

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -eu
 
 VERSIONS=("2.7" "3.5" "3.6" "3.7" "3.8" "3.9" "3.10")
 LATEST="3.10"
-POETRY_VERSION="1.1.11"
+POETRY_VERSION="1.1.12"
 
 error() { printf "\\e[35m[ERROR]\\e[0m %s\\n" "$*" >&2 ; exit 1 ; }
 
