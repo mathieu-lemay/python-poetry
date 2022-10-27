e0fcbb385d39029bef8960ccadeb77bd2fe60b11
daniel <dancrhorton@gmail.com>
2022-10-27 16:48:33 -0400

    simplify templates

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,21 +3,22 @@
 set -eu
 
 VERSION="${1:?}"
-LATEST="3.10"
+LATEST="3.11"
 POETRY_VERSION="1.2.2"
 
 shift
 
 build_options=("--platform" "linux/amd64,linux/arm64" "$@")
 
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}" -f "${VERSION}/Dockerfile" .
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}-slim" -f "${VERSION}/slim/Dockerfile" .
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}-alpine" -f "${VERSION}/alpine/Dockerfile" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}" -f "Dockerfile" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}-slim" -f "Dockerfile.slim" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}-alpine" -f "Dockerfile.alpine" .
 
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}-${POETRY_VERSION}" -f "${VERSION}/Dockerfile" .
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}-slim-${POETRY_VERSION}" -f "${VERSION}/slim/Dockerfile" .
-docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:${VERSION}-alpine-${POETRY_VERSION}" -f "${VERSION}/alpine/Dockerfile" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}-${POETRY_VERSION}" -f "Dockerfile" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}-slim-${POETRY_VERSION}" -f "Dockerfile.slim" .
+docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:${VERSION}-alpine-${POETRY_VERSION}" -f "Dockerfile.alpine" .
 
 if [[ "${VERSION}" == "${LATEST}" ]]; then
-    docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:latest" -f "${LATEST}/Dockerfile" .
+    docker buildx build "${build_options[@]}" --build_arg=PY_VERSION="${VERSION}" -t "acidrain/python-poetry:latest" -f "Dockerfile" .
 fi
+
