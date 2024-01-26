5e0c13a34bd18a56046307704b608e4ea1eb7bdc
Mathieu Lemay <acidrain1@gmail.com>
2024-01-26 13:48:50 -0500

    Switch to CircleCI

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,6 @@ shift
 
 variants=("" "slim" "alpine")
 build_options=(
-    "--platform"
-    "linux/amd64,linux/arm64"
     "--build-arg"
     "PYTHON_VERSION=${PYTHON_VERSION}"
     "--build-arg"
@@ -23,10 +21,10 @@ for variant in "${variants[@]}"; do
     tag="acidrain/python-poetry:${PYTHON_VERSION}${variant:+-${variant}}"
     dockerfile="Dockerfile${variant:+.${variant}}"
 
-    docker buildx build "${build_options[@]}" -t "${tag}" -f "${dockerfile}" .
-    docker buildx build "${build_options[@]}" -t "${tag}-${POETRY_VERSION}" -f "${dockerfile}" .
+    docker build "${build_options[@]}" -t "${tag}" -f "${dockerfile}" .
+    docker build "${build_options[@]}" -t "${tag}-${POETRY_VERSION}" -f "${dockerfile}" .
 done
 
 if [[ "${PYTHON_VERSION}" == "${LATEST}" ]]; then
-    docker buildx build "${build_options[@]}" -t "acidrain/python-poetry:latest" -f "Dockerfile" .
+    docker build "${build_options[@]}" -t "acidrain/python-poetry:latest" -f "Dockerfile" .
 fi
